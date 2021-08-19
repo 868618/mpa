708b051225d12028ad126b6ba53d65a2446c2253
kenny <879464294@qq.com>
2021-08-19 18:25:51 +0800

    ’提交一下‘

--- src/api/index.js
+++ src/api/index.js
@@ -33,7 +33,7 @@ class Api extends Http {
 
   getAliPaySsn(data) {
     return this.request({
-      url: '/cmobile/index.php?app=pay&mod=pay_new_xcx?key=3af2172167b7a3af185f9c7167088019',
+      url: '/cmobile/index.php?app=pay&mod=pay_new_xcx',
       data,
       method: 'post',
     })
