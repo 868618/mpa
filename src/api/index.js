bc31c57de5033c45198656b0d3821b0e17f06d8a
kenny <879464294@qq.com>
2021-08-19 16:22:48 +0800

    ’提交一下‘

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,11 @@
 import Http from '@/utils/http'
 
 class Api extends Http {
-  getDetail(params, headers = { abc: 123 }) {
+  getDetail(params) {
     return this.request({
       url: '/cmobile/index.php?app=userorder&mod=order_info_xcx',
       params,
       method: 'get',
-      headers,
     })
   }
 
@@ -21,8 +20,7 @@ class Api extends Http {
   getAliPayUserId(data, headers = {}) {
     return this.request({
       url: '/api/frontend/member/update_alipay_user_id',
-      data,
-      headers,
+      data: Object.assign(data, headers),
       method: 'post',
     })
   }
