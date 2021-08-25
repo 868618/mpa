fe01140ab8ba90ffb443b4aa06380c26b3ab12b5
kenny <879464294@qq.com>
2021-08-25 11:20:22 +0800

    ’提交一下‘

--- src/utils/tool.js
+++ src/utils/tool.js
@@ -2,7 +2,7 @@ const ua = window.navigator.userAgent.toLowerCase()
 console.log('ua', ua)
 
 const isAliPayApp = () => ua.includes('alipayclient')
-const isWeChat = () => (!!ua.match(/MicroMessenger/ig))
+const isWeChat = () => (ua.match(/MicroMessenger/i) === 'micromessenger')
 
 // eslint-disable-next-line no-param-reassign
 const stringify = obj => Object.entries(obj).reduce((pre, cur) => pre += `&${cur.join('=')}`, '').replace(/^./ig, '?')
